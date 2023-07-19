f48037640f74c0ffd73bb1be376fc43166699b2e
Preytax <pierr_anderrson@outlook.com>
2023-07-19 17:52:00 -0500

    First commit - Pierr

--- src/main/java/com/cineplanet/back/controllers/ctl_usuario.java
+++ src/main/java/com/cineplanet/back/controllers/ctl_usuario.java
@@ -1,12 +1,9 @@
 package com.cineplanet.back.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cineplanet.back.models.mdl_usuarios;
